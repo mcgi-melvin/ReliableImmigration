775d2b36e74bb1943bcab347bc8dd29e2548f7ac
mcgi-melvin <melvinlomibao@gmail.com>
2026-04-03 15:13:53 +0800

    Project Structure

---
+++ wp-content/themes/reliable-immigration/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function includeFilesInFolder( string $folder ): void
+{
+    $files = array_diff( scandir( $folder ), ['..', '.'] );
+
+    foreach ( $files as $file ) {
+        $path = $folder . '/' . $file;
+
+        if( !file_exists( $path ) ) continue;
+
+        if( is_dir( $path ) ) {
+            includeFilesInFolder( $path );
+        } else {
+            require_once $path;
+        }
+    }
+}
+includeFilesInFolder( get_stylesheet_directory() . "/functions" );
